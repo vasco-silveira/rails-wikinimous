7d54978ae2dfe918383fe75d3094ed4894d61de3
Vasco Silveira <vasco.silveira@saltpay.co>
2022-01-15 16:04:24 +0000

    Added 100 seeds with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Books::Dune.quote
+  )
+end
