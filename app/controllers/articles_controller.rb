319899dc6f9b30928bef06643833ef7625f7586e
Vasco Silveira <vasco.silveira@saltpay.co>
2022-01-15 15:49:13 +0000

    Added new, edit and delete with links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,35 +7,35 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-#   def new
-#     @task = Task.new
-#   end
+  def new
+    @article = Article.new
+  end
 
-#   def create
-#     @task = Task.new(task_params)
-#     @task.save
-#     redirect_to task_path(@task)
-#   end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
 
-#   def edit
-#     @task = Task.find(params[:id])
-#   end
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-#   def update
-#     @task = Task.find(params[:id])
-#     @task.update(task_params)
-#     redirect_to task_path(@task)
-#   end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
 
-#   def destroy
-#     @task = Task.find(params[:id])
-#     @task.destroy
-#     redirect_to tasks_path
-#   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
-#   private
+  private
 
-#   def task_params
-#     params.require(:task).permit(:title, :details, :completed)
-#   end
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # Fetch all articles
-  get "articles", to: "articles#index"
-
-  # Fetch one article
-  get "articles/:id", to: "articles#show", as: :article
-
-  # Create new article
+  resources :articles
 end
